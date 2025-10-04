f52056f5f4bd66f67623277c463bf26e0027c5d6
Nexxa <an_na_rib@mail.ru>
2025-10-04 16:11:25 +0700

    Add Onboarding screen version 0.1

--- app/src/main/java/com/example/vk_education/MainActivity.kt
+++ app/src/main/java/com/example/vk_education/MainActivity.kt
@@ -4,14 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.vk_education.R
 import com.example.vk_education.ui.theme.VK_EducationTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             VK_EducationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Onboarding(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +55,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Hello $name!",
-        modifier = modifier
-    )
+fun Onboarding(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.rustore),
+                contentDescription = "RuStore Logo",
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(260 .dp))
+            Text(
+                text = "Это - твой новый магазин приложений для Android",
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "~ Красивая фразочка ~",
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Light,
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            DefaultButton(
+                onClick = { /* TODO: Add action */ }, 
+                text = "Продолжить",
+                width = 200
+            )
+        }
+    }
+}
+
+@Composable
+fun DefaultButton(
+    onClick: () -> Unit,
+    text: String,
+    backgroundColor: Color = Color(0xFF0077FF),
+    width: Int? = null
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(horizontal = 12.dp, vertical = 12.dp)
+            .let { if (width != null) it.width(width.dp) else it },
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor
+        )
+    ) {
+        Text(
+            text = text,
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VK_EducationTheme {
-        Greeting("Android")
+        Onboarding()
     }
 }
